1cb8175bf3e0ad51a0f757ea9df39b0a34e85762
hoanglong040800 <hoanglong040800@gmail.com>
2022-06-29 10:07:02 +0700

    add access & refresh token

--- src/+modules/auth/login.controller.ts
+++ src/+modules/auth/login.controller.ts
@@ -4,6 +4,8 @@ import { TABLE } from 'const'
 import { Request, Response } from 'express'
 import { buildResponse } from 'services'
 import { empty } from 'utils'
+import jwt from 'jsonwebtoken'
+import { Console } from 'console'
 
 interface IReq extends Request {
 	body: {
@@ -15,23 +17,40 @@ interface IReq extends Request {
 interface IResData {
 	user: IUser
 	accessToken: string
+	expire: Date
+	refreshToken: string
 }
+
+const ACCESS_TOKEN_EXPIRE = 30 //minutes
+const REFRESH_TOKEN_EXPIRE = 30 * 60 * 60 * 60
 
 export default async function loginController(req: IReq, res: Response) {
 	try {
 		const { user } = await queryLogic(req)
 
 		if (empty(user)) {
-			return buildResponse(res, 200, null, 'Email or password is incorrect')
+			return buildResponse(res, 400, null, 'Email or password is incorrect')
 		}
 
 		if (user.status !== USER_STATUS.ACTIVE) {
-			return buildResponse(res, 404, null, 'User is inactive')
+			return buildResponse(res, 400, null, 'User is inactive')
 		}
 
+		const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET || '', {
+			expiresIn: ACCESS_TOKEN_EXPIRE,
+		})
+
+		const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET || '', {
+			expiresIn: REFRESH_TOKEN_EXPIRE,
+		})
+
+		const expire = new Date(new Date().getTime() + ACCESS_TOKEN_EXPIRE * 60000)
+
 		const resData: IResData = {
-			user: user,
-			accessToken: '',
+			user,
+			accessToken,
+			expire,
+			refreshToken,
 		}
 
 		return buildResponse(res, 201, resData, 'Login successfully')
